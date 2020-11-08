fb07382e6439ac06fe20352609b5f273538cb8eb
Saerom Bang <saerombang11@gmail.com>
2020-11-08 13:41:18 +0900

    feat: #5 Add tag component
    - Added global styles in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import { addDecorator } from '@storybook/react';
+import React from 'react';
+import { globalStyles } from '../styles/global';
+
+addDecorator((style) => (
+  <>
+    {globalStyles}
+    {style()}
+  </>
+));
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' }
+};
