9f74746b8981b5658b62f63026fba93c71bea62c
Saerom Bang <saerombang11@gmail.com>
2020-11-08 21:20:36 +0900

    feat: #5 Add tag component
    - Added props and styled elements

--- src/components/instance/tag/outlined-tag/outlined-tag.tsx
+++ src/components/instance/tag/outlined-tag/outlined-tag.tsx
@@ -1,21 +1,33 @@
 import React, { FC } from 'react';
+import styled from '@emotion/styled';
 import Div, { DivProps } from '../../../styled-system/div/div';
 import Span from '../../../styled-system/span/span';
 
-const OutlinedTag: FC<DivProps> = (props) => {
+const StyledDiv = styled(Div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid #ffffff;
+  border-radius: 100px;
+  padding: 3px 16px;
+`;
+
+const StyledSpan = styled(Span)`
+  line-height: 18px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #ffffff;
+`;
+
+type OutlinedTagProps = {
+  onClick?: (event: MouseEvent) => void;
+};
+
+const OutlinedTag: FC<OutlinedTagProps> = ({ onClick }) => {
   return (
-    <Div
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      border="2px solid #ffffff"
-      borderRadius="100px"
-      padding="3px 22px"
-      {...props}>
-      <Span color="#FFFFFF" lineHeight="18px" fontSize="14px" fontWeight="500">
-        Coffee
-      </Span>
-    </Div>
+    <StyledDiv>
+      <StyledSpan>Coffee</StyledSpan>
+    </StyledDiv>
   );
 };
 
