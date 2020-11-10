94fa390ca5a30d5ddc61337cfd8bbad8a385b7f5
Saerom Bang <saerombang11@gmail.com>
2020-11-10 14:20:09 +0900

    refactor: #5 Add tag component
    - Deleted styled in solid tag

--- src/components/instance/tag/solid-tag/solid-tag.tsx
+++ src/components/instance/tag/solid-tag/solid-tag.tsx
@@ -1,33 +1,24 @@
 import React, { FC } from 'react';
-import styled from '@emotion/styled';
 import Div, { DivProps } from '../../../styled-system/div/div';
 import Span from '../../../styled-system/span/span';
 
-const StyledDiv = styled(Div)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 100px;
-  padding: 5px 18px;
-  background-color: #ff0d77;
-`;
+// type SolidTagProps = {
+//   onClick?: (event: MouseEvent) => void;
+// };
 
-const StyledSpan = styled(Span)`
-  line-height: 18px;
-  font-size: 14px;
-  font-weight: 500;
-  color: #ffffff;
-`;
-
-type SolidTagProps = {
-  onClick?: (event: MouseEvent) => void;
-};
-
-const SolidTag: FC<SolidTagProps> = ({ onClick }) => {
+const SolidTag: FC<any> = (props) => {
   return (
-    <StyledDiv>
-      <StyledSpan>Coffee</StyledSpan>
-    </StyledDiv>
+    <Div
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="100px"
+      padding="5px 18px"
+      backgroundColor="#ff0d77">
+      <Span lineHeight="18px" fontSize="14px" fontWeight="500" color="#ffffff">
+        Coffee
+      </Span>
+    </Div>
   );
 };
 
