c7ec01b8322b7c5e50d1d3f77514776ad703253b
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-11-12 22:54:50 +0900

    feat: #6 Add place item component
    - Added futura and its fallback fonts into theme object

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,3 +1,7 @@
 export const theme = {
-  breakpoints: ['768px', '1280px']
+  breakpoints: ['768px', '1280px'],
+  fonts: {
+    futura:
+      'Futura PT, -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
+  }
 };
