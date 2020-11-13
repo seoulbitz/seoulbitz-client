ae2b5a638997556baaac74eeeb31a49fad4c1910
Saerom Bang <saerombang11@gmail.com>
2020-11-13 19:32:04 +0900

    refactor: #6 Add place item component
    - Deleted imported font

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,11 +1,9 @@
-import { nanumsquare } from './fonts/nanum-square';
-
 export const theme = {
   breakpoints: ['768px', '1280px'],
   fonts: {
     futura:
       'Futura PT, -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
     nanumsquare:
-      'NanumSquare , -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
+      'nanumsquare , -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
   }
 };
