cb96713b958244d7ab28d4cd351a72ae3feeb02f
Saerom Bang <saerombang11@gmail.com>
2020-11-13 12:19:28 +0900

    refactor: #6 Add place item component
    - Deleted unnecessary fonts

--- src/styles/fonts/nanum-square.tsx
+++ src/styles/fonts/nanum-square.tsx
@@ -1,16 +1,6 @@
 import { css } from '@emotion/core';
 
 export const nanumsquare = css`
-  /* @font-face {
-    font-family: 'NanumSquare';
-    font-weight: 400;
-    font-style: 'light';
-    src: local('NanumSquare') ('/fonts/nanum-square/NanumSquareL.eot'),
-      url('/fonts/nanum-square/NanumSquareL.eot?#iefix') format('embedded-opentype'),
-      url('/fonts/nanum-square/NanumSquareL.woff') format('woff'),
-      url('/fonts/nanum-square/NanumSquareL.ttf') format('ttf');
-  } */
-
   @font-face {
     font-family: 'NanumSquare';
     font-weight: 800;
@@ -20,14 +10,4 @@ export const nanumsquare = css`
       url('/fonts/nanum-square/NanumSquareR.woff') format('woff'),
       url('/fonts/nanum-square/NanumSquareR.ttf') format('ttf');
   }
-  /* 
-  @font-face {
-    font-family: 'NanumSquare';
-    font-weight: 800;
-    font-style: 'bold';
-    src: local('NanumSquare') ('/fonts/nanum-square/NanumSquareB.eot'),
-      url('/fonts/nanum-square/NanumSquareB.eot?#iefix') format('embedded-opentype'),
-      url('/fonts/nanum-square/NanumSquareB.woff') format('woff'),
-      url('/fonts/nanum-square/NanumSquareB.ttf') format('ttf');
-  } */
 `;
