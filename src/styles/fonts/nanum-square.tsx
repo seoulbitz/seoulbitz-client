3ab016bba064442a4f56bae66a0cab56f50d8456
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-11-14 20:51:29 +0900

    feat: #6 Add place item component
    - Added nanumsquare fonts

--- src/styles/fonts/nanum-square.tsx
+++ src/styles/fonts/nanum-square.tsx
@@ -1,13 +1,50 @@
 import { css } from '@emotion/core';
 
-export const nanumsquare = css`
+export const nanumSquare = css`
+  @font-face {
+    font-family: 'NanumSquare';
+    font-weight: 300;
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareL.eot');
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareL.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareL.woff')
+        format('woff'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareL.ttf')
+        format('truetype');
+  }
+  @font-face {
+    font-family: 'NanumSquare';
+    font-weight: 400;
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareR.eot');
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareR.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareR.woff')
+        format('woff'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareR.ttf')
+        format('truetype');
+  }
+
+  @font-face {
+    font-family: 'NanumSquare';
+    font-weight: 700;
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareB.eot');
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareB.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareB.woff')
+        format('woff'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareB.ttf')
+        format('truetype');
+  }
+
   @font-face {
     font-family: 'NanumSquare';
     font-weight: 800;
-    font-style: 'medium';
-    src: local('NanumSquare') ('/fonts/nanum-square/NanumSquareR.eot'),
-      url('/fonts/nanum-square/NanumSquareR.eot?#iefix') format('embedded-opentype'),
-      url('/fonts/nanum-square/NanumSquareR.woff') format('woff'),
-      url('/fonts/nanum-square/NanumSquareR.ttf') format('ttf');
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareEB.eot');
+    src: url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareEB.eot?#iefix')
+        format('embedded-opentype'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareEB.woff')
+        format('woff'),
+      url('https://seoulbitz-client-4f37fzqy4.vercel.app/fonts/nanum-square/NanumSquareEB.ttf')
+        format('truetype');
   }
 `;
