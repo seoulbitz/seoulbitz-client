f224165cb9c27a57f7228f47437eb6b8195ba90b
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-11-15 15:10:37 +0900

    feat: #19 Add layout for places page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
-
-const Home = () => {
-  return (
-    <>
-      <div>안녕!</div>
-    </>
-  );
-};
-
-export default Home;
+export { default } from '@/components/pages/places/places';
