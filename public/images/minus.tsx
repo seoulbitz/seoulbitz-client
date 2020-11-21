7ccbf96d27457b4d87b73a85eeda2d36444bda4d
Saerom Bang <saerombang11@gmail.com>
2020-11-22 00:17:57 +0900

    feat: #27 Create FAQ page markups
    Added svg images

---
+++ public/images/minus.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+function Minus(props) {
+  return (
+    <svg
+      width={16}
+      height={16}
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <path fill="#0511F2" d="M.5 7h15v2H.5z" />
+    </svg>
+  );
+}
+
+export default Minus;
