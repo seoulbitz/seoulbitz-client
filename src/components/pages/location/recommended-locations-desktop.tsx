214e099048c52554c528bbc8b50e6f7fdf562c8e
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-11-29 13:08:51 +0900

    feat: #25 Create place page
    - Added location page markup

---
+++ src/components/pages/location/recommended-locations-desktop.tsx
@@ -0,0 +1,116 @@
+import styled from '@emotion/styled';
+import Div from '@/components/styled-system/div/div';
+import React, { FC, useState } from 'react';
+import * as SwiperReact from 'swiper/react';
+import Span from '@/components/styled-system/span/span';
+import { theme } from '@/styles/theme';
+import Swiper from 'swiper';
+
+const { Swiper: SwiperComponent, SwiperSlide } = SwiperReact;
+
+const LocationItem = () => {
+  return (
+    <Div>
+      <Div display={[null, null, null]} position="relative" paddingTop="75%">
+        <Div
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          backgroundSize="cover"
+          backgroundImage="url('https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/e35/57286270_114670046398227_2576349361528457787_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=103&_nc_ohc=js3DIwAOIPkAX8dsjiC&tp=18&oh=76cb6bf0910fc3ef3ed12303dcb65cbe&oe=5FD47285')"
+          backgroundPosition="center"
+          // borderRadius="8px"
+        />
+      </Div>
+      <Div color="#080CCE" marginTop="16px">
+        <Span
+          fontFamily={theme.fonts.nanumSquare}
+          fontSize="16px"
+          lineHeight="20px"
+          fontWeight="800">
+          {'일독일박'},{' '}
+        </Span>
+        <Span fontFamily={theme.fonts.futura} fontSize="16px" lineHeight="20px" fontWeight="700">
+          {'Of. one book stay'}
+        </Span>
+      </Div>
+      <Div
+        display={[null, null, null]}
+        fontFamily={theme.fonts.futura}
+        fontWeight="400"
+        marginTop="8px"
+        fontSize="16px"
+        lineHeight="20px">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      </Div>
+    </Div>
+  );
+};
+
+const Wrapper = styled(Div)`
+  .swiper-container {
+    padding-bottom: 0px;
+  }
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    display: none;
+  }
+
+  .swiper-pagination {
+    display: none;
+  }
+`;
+
+const RecommendedLocationsDesktop: FC<any> = (props) => {
+  // const [controlledSwiper, setControlledSwiper] = useState(null);
+  const [swiper, setSwiper] = useState<Swiper>(null);
+
+  const handleSlidePrev = () => {
+    swiper.slidePrev();
+  };
+
+  const handleSlideNext = () => {
+    swiper.slideNext();
+  };
+
+  return (
+    <Wrapper>
+      <SwiperComponent
+        spaceBetween={24}
+        slidesPerView={3}
+        slidesPerGroup={3}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => {
+          setSwiper(swiper);
+        }}>
+        <SwiperSlide>
+          <LocationItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LocationItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LocationItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LocationItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LocationItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LocationItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <LocationItem />
+        </SwiperSlide>
+      </SwiperComponent>
+    </Wrapper>
+  );
+};
+
+export default RecommendedLocationsDesktop;
