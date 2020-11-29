228f20d19dfa6af18aef35ee0fd7232eb60b2aa4
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-11-29 13:07:51 +0900

    feat: #25 Create place page
    - Changed font family name on theme

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -2,10 +2,10 @@ export const theme = {
   breakpoints: ['768px', '1280px'],
   fonts: {
     futura:
-      'Futura PT, -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
+      'Futura, -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
     nanumSquare:
-      'NanumSquare , -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
+      'NanumSquare, -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
     adelle:
-      'Adelle , -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
+      'Adelle, -apple-system, BlinkMacSystemFont, Segoe UI, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
   }
 };
