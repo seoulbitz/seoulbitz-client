39d579588c7b8905e42421ef7e29ccd58d4a2bfd
Saerom Bang <saerombang11@gmail.com>
2020-12-09 12:03:51 +0900

    fix: #35 Create locations page
    Fixed toggle

--- src/components/pages/locations/locations-toggle.tsx
+++ src/components/pages/locations/locations-toggle.tsx
@@ -1,10 +1,17 @@
 import Clock from '@/components/icons/clock/clock';
+import WhiteClock from '@/components/icons/clock/white-clock';
 import Distance from '@/components/icons/distance/distance';
+import WhiteDistance from '@/components/icons/distance/white-distance';
 import Heart from '@/components/icons/heart/heart';
+import WhiteHeart from '@/components/icons/heart/white-heart';
 import Div from '@/components/styled-system/div/div';
 import Span from '@/components/styled-system/span/span';
 import { theme } from '@/styles/theme';
 import React, { useState } from 'react';
+
+const StyledDistance = () => {
+  <Distance color="#ffffff"></Distance>;
+};
 
 const LocationsToggle = () => {
   const [state, setState] = useState<'latest' | 'likes' | 'distance'>('latest');
@@ -19,23 +26,64 @@ const LocationsToggle = () => {
         <Span fontFamily={theme.fonts.futura} fontSize="16px" lineHeight="22px" fontWeight="500">
           Sort by
         </Span>
-        <Span
-          marginLeft="2px"
-          fontFamily={theme.fonts.futura}
-          fontSize="16px"
-          lineHeight="22px"
-          fontWeight="500"
-          color="#0511F2">
-          latest
-        </Span>
+        {state === 'distance' && (
+          <Span
+            marginLeft="2px"
+            fontFamily={theme.fonts.futura}
+            fontStyle="italic"
+            fontSize="16px"
+            lineHeight="22px"
+            fontWeight="500"
+            color="#0511F2">
+            distance
+          </Span>
+        )}
+        {state === 'latest' && (
+          <Span
+            marginLeft="2px"
+            fontFamily={theme.fonts.futura}
+            fontStyle="italic"
+            fontSize="16px"
+            lineHeight="22px"
+            fontWeight="500"
+            color="#0511F2">
+            latest
+          </Span>
+        )}
+        {state === 'likes' && (
+          <Span
+            marginLeft="2px"
+            fontFamily={theme.fonts.futura}
+            fontStyle="italic"
+            fontSize="16px"
+            lineHeight="22px"
+            fontWeight="500"
+            color="#0511F2">
+            likes
+          </Span>
+        )}
       </Div>
       <Div
         display="flex"
+        justifyContent="center"
         alignItems="center"
-        justifyContent="space-around"
         border="2px solid #0511F2"
         borderRadius="29px"
-        marginTop="16px">
+        marginTop="16px"
+        width="160px"
+        height="56px">
+        <Div
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          onClick={handleDistanceClick}
+          width="48px"
+          height="48px"
+          margin="2px"
+          borderRadius="100px"
+          backgroundColor={state === 'distance' ? '#0511F2' : 'initial'}>
+          {state === 'distance' ? <WhiteDistance /> : <Distance />}
+        </Div>
         <Div
           display="flex"
           justifyContent="center"
@@ -46,19 +94,7 @@ const LocationsToggle = () => {
           margin="2px"
           borderRadius="100px"
           backgroundColor={state === 'latest' ? '#0511F2' : 'initial'}>
-          Latest
-        </Div>
-        <Div
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          onClick={handleDistanceClick}
-          width="48px"
-          height="48px"
-          margin="2px"
-          borderRadius="100px"
-          backgroundColor={state === 'latest' ? '#0511F2' : 'initial'}>
-          Distance
+          {state === 'latest' ? <WhiteClock /> : <Clock />}
         </Div>
         <Div
           display="flex"
@@ -69,8 +105,8 @@ const LocationsToggle = () => {
           height="48px"
           margin="2px"
           borderRadius="100px"
-          backgroundColor={state === 'latest' ? '#0511F2' : 'initial'}>
-          Likes
+          backgroundColor={state === 'likes' ? '#0511F2' : 'initial'}>
+          {state === 'likes' ? <WhiteHeart /> : <Heart />}
         </Div>
       </Div>
     </>
