512e1e5bbc18ed0ad5939656a909a24ed4018d9f
Saerom Bang <saerombang11@gmail.com>
2020-12-13 15:18:26 +0900

    fix: #36 Change font size in FAQ page
    - Added Layout in Faq page
    - Changed name convention

--- src/components/faq-item/faq-item.tsx
+++ src/components/faq-item/faq-item.tsx
@@ -1,8 +1,7 @@
 import { theme } from '@/styles/theme';
-import styled from '@emotion/styled';
 import React, { useState } from 'react';
-import Close from '../../../public/images/close';
-import Minus from '../../../public/images/minus';
+import Minus from '../icons/minus/minus';
+import Plus from '../icons/plus/plus';
 import Div from '../styled-system/div/div';
 import P from '../styled-system/p/p';
 
@@ -15,16 +14,14 @@ const FAQItem = () => {
 
   return (
     <>
-      <QuestionBox
-        style={{
-          cursor: 'pointer'
-        }}
+      <Div
+        cursor="pointer"
         onClick={handleClick}
         display="flex"
         alignItems="center"
         justifyContent="space-between"
         borderBottom={isOpen ? '0px' : '1px solid #000000'}
-        padding={['23px 0', null, '22px 0']}>
+        padding="22px 0">
         <Div
           display="flex"
           alignItems="flex-end"
@@ -36,29 +33,26 @@ const FAQItem = () => {
           Pater noster qui es in caelis
         </Div>
         <Div display="flex" alignItems="flex-end" verticalAlign="text-top">
-          {isOpen ? <Minus /> : <Close />}
+          {isOpen ? <Minus /> : <Plus />}
         </Div>
-      </QuestionBox>
+      </Div>
       {isOpen && (
         <Div paddingBottom={['24px', null, '32px']} borderBottom="1px solid">
           <P
             margin="0px"
             fontFamily={theme.fonts.adelle}
-            fontSize={'16px'}
-            lineHeight={'24px'}
+            fontSize="16px"
+            lineHeight="24px"
             fontWeight="400">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna.
           </P>
         </Div>
       )}
     </>
   );
 };
-
-const QuestionBox = styled(Div)`
-  cursor: pointer;
-`;
 
 export default FAQItem;
--- src/components/pages/faq/faq.tsx
+++ src/components/pages/faq/faq.tsx
@@ -1,147 +1,47 @@
 import Div from '@/components/styled-system/div/div';
-import Header from '@/components/styled-system/header/header';
-import Aside from '@/components/styled-system/aside/aside';
 import { theme } from '@/styles/theme';
-import { ThemeProvider } from 'emotion-theming';
 import React, { FC } from 'react';
-import Main from '@/components/styled-system/main/main';
 import { Cell, Grid } from '@/components/content/layout-grid/layout-grid';
 import Img from '@/components/styled-system/img/img';
 import FAQItem from '@/components/faq-item/faq-item';
-import ChangeFaq from './change-faq';
-import QuestionBox from '@/components/instance/question-box/question-box';
-import AnswerBox from '@/components/instance/answer-box/answer-box';
+import Layout from '@/components/layout/layout';
 
-const MenuBar = () => {
-  return <Div height="56px" backgroundColor="#ffffff"></Div>;
-};
-
-const FlowBar = () => {
-  return <Div height="28px" backgroundColor="#000000"></Div>;
-};
-
-const DesktopHeader = () => {
+const FAQ: FC = () => {
   return (
-    <Div
-      height="100%"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      padding="0 24px">
-      <Div width="240px" height="48px" borderBottom="1px solid #0511F2"></Div>
-      <Div display="flex">
-        <Div width="108px" height="48px" border="1px solid #0511F2" marginRight="16px"></Div>
-        <Div width="67px" height="48px" border="1px solid #000000"></Div>
-      </Div>
-    </Div>
-  );
-};
-
-const SideBar = () => {
-  return (
-    <>
-      <Div height="88px" backgroundColor="#999999"></Div>
-      <Div height="80px" backgroundColor="#ffffff"></Div>
-      <Div height="196px" backgroundColor="#080CCE"></Div>
-      <Div height="208px" backgroundColor="#EBEDED"></Div>
-      <Div height="168px" backgroundColor="#ffffff" borderBottom="1px solid #000000"></Div>
-      <Div height="80px" backgroundColor="#ffffff"></Div>
-      <Div
-        height="56px"
-        backgroundColor="#ffffff"
-        borderTop="1px solid #080CCE"
-        borderBottom="1px solid #080CCE"></Div>
-      <Div height="500px" backgroundColor="#F5F3F0"></Div>
-    </>
-  );
-};
-
-const FAQContent = () => {
-  return (
-    <Grid width="100%" justifyContent="center">
-      <Cell width={['100%', null, 10 / 12]}>
-        <Div
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          marginTop={['48px', null, '88px']}
-          fontFamily={theme.fonts.futura}
-          fontSize="28px"
-          lineHeight="34px"
-          fontWeight="700"
-          color="#000000">
-          FAQ
-        </Div>
-        <Div display="flex" justifyContent="center" alignItems="center" marginTop="32px">
-          <Img width="150px" src="https://via.placeholder.com/300x188" />
-        </Div>
-        <Div marginTop={['40px', null, '48px']}>
-          <FAQItem />
-          <FAQItem />
-          <FAQItem />
-          <FAQItem />
-          <FAQItem />
-          <FAQItem />
-          <FAQItem />
-          {/* <QuestionBox />
-          <QuestionBox />
-          <QuestionBox />
-          <QuestionBox />
-          <AnswerBox />
-          <QuestionBox />
-          <QuestionBox /> */}
-        </Div>
-      </Cell>
-    </Grid>
-  );
-};
-
-const FAQ = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Div display="flex" flexDirection="column" minHeight="100%">
-        {/* Mobile Header */}
-        <Div display={[null, null, 'none']}>
-          <Header position="fixed" top="0" height="84px" width="100%">
-            <MenuBar />
-            <FlowBar />
-          </Header>
-        </Div>
-
-        {/* Desktop Header */}
-        <Div display={['none', null, 'block']}>
-          <Header
-            position="fixed"
-            top={0}
-            left={0}
-            height="88px"
-            paddingLeft="392px"
-            width="100%"
-            backgroundColor="#ffffff">
-            <DesktopHeader />
-          </Header>
-        </Div>
-
-        <Div display="flex">
-          {/* Sidebar */}
-          <Div display={['none', null, 'block']} minWidth="392px">
-            <Aside position="fixed" top={0} left={0} bottom={0} width="392px" display="flex">
-              <Div width="100%" overflowY="auto">
-                <SideBar />
-              </Div>
-              <Div width="32px" backgroundColor="#000000" height="100%"></Div>
-            </Aside>
+    <Layout>
+      <Grid width="100%" justifyContent="center">
+        <Cell width={['100%', null, 10 / 12]}>
+          <Div
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            marginTop={['48px', null, '88px']}
+            fontFamily={theme.fonts.futura}
+            fontSize="28px"
+            lineHeight="34px"
+            fontWeight="700"
+            color="#000000">
+            FAQ
           </Div>
-
-          {/* Content */}
-          <Div width="100%" marginTop={['84px', null, '96px']}>
-            <Main display="flex" flexDirection="column">
-              <FAQContent />
-            </Main>
+          <Div
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            marginTop="32px">
+            <Img width="150px" src="https://via.placeholder.com/300x188" />
           </Div>
-        </Div>
-      </Div>
-    </ThemeProvider>
+          <Div marginTop={['40px', null, '48px']}>
+            <FAQItem />
+            <FAQItem />
+            <FAQItem />
+            <FAQItem />
+            <FAQItem />
+            <FAQItem />
+            <FAQItem />
+          </Div>
+        </Cell>
+      </Grid>
+    </Layout>
   );
 };
 
