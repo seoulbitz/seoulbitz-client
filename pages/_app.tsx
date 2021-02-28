d784c1d378a6a2f9899cc8e37f2f9b169adc04af
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2021-02-28 14:50:34 +0900

    feat: 130 Add welcome page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,37 @@ const App = ({ Component, pageProps }: AppProps) => {
     };
   }, [router.events]);
 
+  useEffect(() => {
+    // 로컬 스토리지의 already-visited 값 불러오기
+    const itemValue = localStorage.getItem('already-visited');
+
+    // 이미 방문한 적 있는데, welcome페이지에 접근헀으면 메인으로 이동
+    if (router.pathname === '/welcome' && itemValue === 'true') {
+      router.replace({
+        pathname: '/'
+      });
+      return;
+    }
+
+    // 이미 방문한 적 있으면 아무것도 안함
+    if (itemValue === 'true') {
+      return;
+    }
+
+    // 한번도 방문한 적 없고 welcome 페이지에 왔을 떄, 아무것도 안함
+    if (router.pathname === '/welcome') {
+      return;
+    }
+
+    // 한번도 방문한 적 없으면 welcome 페이지로 이동
+    router.replace({
+      pathname: '/welcome',
+      query: {
+        redirect: router.asPath
+      }
+    });
+  }, []);
+
   return (
     <CacheProvider value={cache}>
       {globalStyles}
